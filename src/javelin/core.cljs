4664a141f7fc98fd01eb0860dfde2e73f9c0729d
Ben Wen <github@benwen.com>
2017-07-08 16:57:46 -0700

    Most rudimentary posting of markdown->html to s3 via Lambda.

--- src/javelin/core.cljs
+++ src/javelin/core.cljs
@@ -12,123 +12,101 @@
             [promesa.core :as p])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(def markdown-it (nodejs/require "markdown-it"))
-(def md (new markdown-it))
+(declare fetch-last-post post-doc post-docs verify-docs post-last-post)
 
+(def markdown-it (nodejs/require "markdown-it"))
 (def s3 (nodejs/require "s3"))
+(def fs (nodejs/require "fs"))          ; To read and write /tmp
+
+(def md (new markdown-it))
 (def s3-client (.createClient s3 (clj->js {})))
 
-(def fs (nodejs/require "fs"))          ; We need fs later to read write and /tmp
-(def config
-  (-> (.readFileSync fs "static/config.edn" "UTF-8")
-      read-string))
+;;
+;; This is the function that AWS Lambda will call.
+;;
+(deflambda work-magic [event ctx]
+  ;; There are three async operations that cascade upon one another
+  ;; 1. Fetching last-post.edn from s3   TODO this is not multi-lambda instance safe!  Fix it.
+  ;; 2. (this can be parallelized) Posting each document to s3
+  ;; 3. Posting the new last-post.edn to s3
+  (let [retval (p/alet [last-post        (p/await (fetch-last-post))
+                        posted-docs      (p/await (post-docs last-post event ctx))
+                        verified-docs    (p/await (verify-docs posted-docs))
+                        posted-last-post (p/await (post-last-post (+ last-post verified-docs)))]
+                       posted-last-post)]
+    retval))
 
-;(def bb (nodejs/require "bluebird"))
+;;
+;; SUPPORT FUNCTIONS
+;;
 
-;; The counter for the current document file name suffix is in an s3
-;; object
+;; The counter for the last-post filename's suffix is in an s3 object.
 ;; TODO We'll need to hash it? or try serializing with a global semaphore.
-(def current-tmp "/tmp/current.edn")
-(def s3-current-counter-params {:localFile current-tmp
-                                :s3Params {:Bucket "javelintest"
-                                           :Key "current.edn"}})
+(def last-post-tmp "/tmp/last-post.edn")
+(def s3-last-post-counter-params {:localFile last-post-tmp
+                                  :s3Params {:Bucket "javelintest"
+                                             :Key "last-post.edn"}})
 
 ;; TODO use the Bluebird Promise.promisify facility to transform this callback API into a Promise one.
-;; FIXME why do I call the file "current" but the keyword is :last-post.  They should be the same.
 ;; Learning note: the instantiation of the downloader object needs to also be wrapped by the p/promise
 ;; so errors on instantiation that may be thrown are caught.
-(defn fetch-current
-  "Fetches the current.edn file from s3, asynchronously. Resolves to the number of the last post."
+(defn fetch-last-post
+  "Fetches the last-post.edn file from s3, asynchronously. Resolves to the number of the last post."
   []
   (p/promise (fn [resolve reject]
-               (let [downloader (.downloadFile s3-client (clj->js s3-current-counter-params))]
-                 (.on downloader "end" #(let [current-obj (->> (.readFileSync fs current-tmp "utf8")
-                                                               read-string)
-                                              value (:last-post (js->clj current-obj))]
-                                          (js/console.log (str/join ["Got current.edn. Last-post = " value]))
+               (let [downloader (.downloadFile s3-client (clj->js s3-last-post-counter-params))]
+                 (.on downloader "end" #(let [last-post-obj (->> (.readFileSync fs last-post-tmp "utf8")
+                                                                 read-string)
+                                              value (:last-post (js->clj last-post-obj))]
+                                          (js/console.log (str/join ["Old last-post = " value]))
                                           (resolve value)))
                  (.on downloader "error" #(do
                                             (js/console.log (str/join ["Got error = " %]))
                                             (reject {:error %})))))))
 
 (defn post-doc
-  "Computes html and posts record to s3 using document number docnum, asynchronously.  Returns a promise."
+  "Computes html and posts record to s3 using document number docnum, asynchronously.  Resolves to a keyword :success."
   [record docnum]
-  (let [my-name (gstring/format "document%07d.html" docnum)
-        local-file (str/join ["/tmp/" my-name]) 
-        s3-params {:localFile local-file
-                   :s3Params {:Bucket "javelintest"
-                              :Key my-name}}
-        my-text (.render md (str (b64/decodeString (-> record :kinesis :data))))
-        _ignore (.writeFileSync fs local-file my-text)
-        uploader (.uploadFile s3-client (clj->js s3-params))]
-    (js/console.log my-text)
-
-    (p/promise (fn [resolve reject]
+  (p/promise (fn [resolve reject]
+               (let [my-name (gstring/format "document%06d.html" docnum)
+                     local-file (str/join ["/tmp/" my-name]) 
+                     s3-params {:localFile local-file
+                                :s3Params {:Bucket "javelintest"
+                                           :Key my-name}}
+                     my-text (.render md (str (b64/decodeString (-> record :kinesis :data))))
+                     _ignore (.writeFileSync fs local-file my-text)
+                     uploader (.uploadFile s3-client (clj->js s3-params))]
                  (.on uploader "end" (fn [] (resolve :success)))
                  (.on uploader "error" (fn [r] (reject {:error r})))))))
 
 (defn post-docs
-  "Walks Records, calling post-doc, asynchronously.  Returns a list of status."
-  [current {:keys [Records] :as event} ctx]
-  (let [next (+ current 1)
-        last (+ current (count Records))]
-    ;; It is not clear to me if I need the doall here. In Clojure it would be needed to force what would be
-    ;; a lazy sequence.  The https://www.clojurescript.org/about/differences was not helpful.  And a naive
-    ;; test suggested that it was *not* needed, maybe because it's a return value.
-    ;; TODO figure this out, until then be safer with doall included
-    (p/all (doall (map post-doc Records (range next (+ 1 last)))))))
+  "Walks Records, calling post-doc, asynchronously.  Resolves to a list of statuses."
+  [last-post {:keys [Records] :as event} ctx]
+  (let [next (+ last-post 1)
+        new-last (+ last-post (count Records))]
+    (p/all (map post-doc Records (range next (+ 1 new-last))))))
 
 (defn verify-docs
-  "Checks that this is a list of status :success. 
-   Returns the length of the list if ok. Raises an exception otherwise."
+  "Checks that this is a list of status :success, asynchronously.
+   Resolves to the length of the list if ok. Rejects otherwise.
+   Wrapped in a promise so it can participate in a p/alet."
   [docs-status]
-  (if (every? #(= :success %) docs-status)
-    (count docs-status)
-    (throw (js/Error. "Save doc(s) failed. " docs-status))))
-
-(defn post-current
-  "Posts (writes) back a new current.edn file to s3, asynchronously."
-  [new-current]
-  ;; Store back current number
-  (.writeFileSync fs current-tmp (pr-str {:last-post new-current}))
   (p/promise (fn [resolve reject]
-               (let [uploader (.uploadFile s3-client (clj->js s3-current-counter-params))]
-                 (.on uploader "end" (fn [] (resolve :success)))
+               (if (every? #(= :success %) docs-status)
+                 (resolve (count docs-status))
+                 (reject {:failed-post-docs docs-status})))))
+
+(defn post-last-post
+  "Posts (writes) back a new last-post.edn file to s3, asynchronously.
+   Resolves to the new last-post number."
+  [new-last-post]
+  ;; Store back last-post number
+  (p/promise (fn [resolve reject]
+               (.writeFileSync fs last-post-tmp (pr-str {:last-post new-last-post}))
+               (let [uploader (.uploadFile s3-client (clj->js s3-last-post-counter-params))]
+                 (.on uploader "end" (fn []
+                                       (js/console.log (str/join ["New last-post = " new-last-post]))
+                                       (resolve new-last-post)))
                  (.on uploader "error" (fn [r] (reject {:error r})))))))
    
-(defn test-async-DELETEME 
-  [{:keys [msecs] :or {msecs 1000}} ctx]
-  (p/promise
-   (fn [resolve]
-     (p/schedule msecs #(resolve {:waited msecs})))))
 
-;(def s3-downloadFile-async (.promisify bb (. s3-client downloadFile)))
-
-(defn fetch-current-TOY-DELETEME
-  "Fetches the current.edn file from s3, asynchronously. Resolves to the number of the last post."
-  []
-  (js/console.log "in fetch-current")
-  (-> (s3-downloadFile-async (clj->js s3-current-counter-params))
-      (p/then (fn []
-                (js/console.log (str/join ["file" (-> (.readFileSync fs current-tmp)
-                                                      read-string)]))))
-      (p/catch (fn [error] (js/console.log error))))
-  )
-
-(deflambda work-magic [event ctx]
-  ;; There are three async operations that cascade upon one another
-  ;; 1. Fetching current.edn from s3   TODO this is not multi-lambda instance safe!  Fix it.
-  ;; 2. (this can be parallelized) Posting each document to s3
-  ;; 3. Posting the new current.edn to s3
-  (js/console.log "hello")
-  (fetch-current)
-;;  (test-async-DELETEME {:msecs 300})
-  ;; (let [retval (p/alet [current        (p/await (fetch-current))
-  ;;                       posted-docs    (p/await (post-docs current event ctx))
-  ;;                       verified-docs  (verify-docs posted-docs)
-  ;;                       posted-current (p/await (post-current (+ current verified-docs)))]
-  ;;                      (js/console.log "current " current)   
-  ;;                      posted-current)]
-  ;;   @retval)                            ; IM HERE Huh, why can I not resolve the p/alet implicit promise?
-)
